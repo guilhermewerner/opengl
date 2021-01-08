cacb232a42b2a3ea665863364face431f82de955
GuilhermeWerner <guilhermeqwerner@gmail.com>
2021-01-08 18:26:49 -0300

    Setup GLEW

--- OpenGL/Source/Application.cpp
+++ OpenGL/Source/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main()
 {
@@ -18,6 +21,13 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error!" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
